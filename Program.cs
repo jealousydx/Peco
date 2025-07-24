108f7317f81daa64151fda798fd6ddee1d81ed61
jealousydx <146007893+jealousydx@users.noreply.github.com>
2025-07-25 00:50:06 +0300

    added mutex

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@ namespace Peco
         [STAThread]
         static void Main()
         {
+            _ = new Mutex(true, "Global\\Peco", out bool isFree);
+
+            if (!isFree)
+            {
+                Alert.Error("The program has already running");
+                return;
+            }
+
             ApplicationConfiguration.Initialize();
 
             App app = new();
